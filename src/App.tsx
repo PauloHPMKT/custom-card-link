7c75f5b15155d03e2e8df71966a62e51bc8f2748
Paulo Sergio <pauloserg861@gmail.com>
2024-10-05 12:08:42 -0300

    feat: implement title update

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useRef, useState } from "react";
 import { LuLink } from "react-icons/lu";
 import { IoMdList } from "react-icons/io";
-
+import { MdSubtitles, MdClose, MdCheck } from "react-icons/md";
 
 export function App() {
   const contentRef = useRef<HTMLDivElement>(null);
+  const [title, setTitle] = useState("");
   const [url, setUrl] = useState("");
   const [linkText, setLinkText] = useState("");
   const [isEditable, setIsEditable] = useState(true);
@@ -32,7 +33,6 @@ export function App() {
     link.textContent = text || url;
     link.target = "_blank";
     link.rel = "noopener noreferrer";
-
     return link;
   }
 
@@ -44,18 +44,11 @@ export function App() {
       const linkElement = createLink(url, linkText);
       const customizedLink = linkCustomization(linkElement);
 
-      // Atribua o comportamento do link com base no `isEditable`6
       if (isEditable) {
-        // Se `isEditable` for `true`, impede navegação e exibe alerta
         customizedLink.onclick = (e) => {
-          console.log('Comportamento editável ativado:', isEditable);
           e.preventDefault();
           alert(`Link: ${linkElement.href}\nTexto: ${linkElement.textContent}`);
         };
-      } else {
-        // Se `isEditable` for `false`, permite navegação normal
-        customizedLink.onclick = null;
-        console.log('Navegação normal permitida:', isEditable);
       }
 
       contentElement.appendChild(customizedLink);
@@ -68,20 +61,17 @@ export function App() {
     }
   };
 
-  // Função para gerenciar cliques fora do conteúdo editável
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+  
   const handleClickOutside = (event: MouseEvent) => {
     console.log('ta entrando na função?')
     const contentElement = contentRef.current;
     const inputElement = document.querySelector("#input-container");
     
     if (contentElement || inputElement) {
-      // Se clicou fora do conteúdo e fora dos inputs, desativa edição
       if (!contentElement?.contains(event.target as Node) && !inputElement?.contains(event.target as Node)) {
         console.log('se tiver entrando, ta passando por aqui?')
         setIsEditable(false);
       }
-      // Se clicou dentro do conteúdo (fora de links), ativa edição
       else if (!isEditable && !(event.target as Element)?.closest("a")) {
         console.log('ou ta passando por aqui?')
         setIsEditable(true);
@@ -93,7 +83,6 @@ export function App() {
   useEffect(() => {
     const contentElement = contentRef.current;
     if (contentElement) {
-      // Seleciona todos os links dentro do conteúdo
       const links = contentElement.querySelectorAll("a");
       links.forEach((link) => {
         if (isEditable) {
@@ -106,7 +95,7 @@ export function App() {
         }
       });
     }
-  }, [isEditable]); // Reexecuta sempre que `isEditable` mudar
+  }, [isEditable]);
 
   useEffect(() => {
     document.addEventListener("mousedown", handleClickOutside);
@@ -118,29 +107,58 @@ export function App() {
   return (
     <div className="p-8 relative bg-[#1a1d21] h-screen text-[#b6b2cf]">
       <div className="absolute w-[700px] shadow-xl py-6 px-8 rounded-lg bg-[#323940]">
-        <h2 className="text-2xl font-bold mb-6">Titulo do card</h2>
-        {isEditable}
+        <div className="flex w-full">
+          <MdSubtitles size={24} className="mr-4 mt-2" />
+          <div className="w-full">
+            <div className="p-1 mb-4 flex w-full rounded-sm overflow-hidden">
+              <input
+                type="text"
+                className={
+                  `rounded-sm resize-none outline-none 
+                  ${title.length >= 1 ? 'w-[85%]' : 'w-full'} 
+                  bg-[#323940] text-2xl font-bold h-9 overflow-hidden focus:outline-none focus:ring-2 focus:ring-blue-500`
+                }
+                value={title || "Defina um título para o card"}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+              {title.length >= 1 && (
+                <div className="w-[15%] flex gap-1 ml-1">
+                  <button className="bg-slate-400 rounded-sm h-full w-1/2 flex justify-center items-center">
+                    <MdCheck color="#0aaf0d" size={20} />
+                  </button>
+                  <button className="bg-slate-400 rounded-sm h-full w-1/2 flex justify-center items-center">
+                    <MdClose color="#ff0000" size={20} />
+                  </button>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
         <div className="flex items-start">
           <IoMdList size={24} className="mt-1 mr-4" />
           <h3 className="font-semibold mb-4">Descrição</h3>
         </div>
         <div className="ml-10 relative">
-          {showContentEditable 
-            ? (
-              <div className="rounded-md overflow-hidden border-2">
+          {showContentEditable ? (
+            <>
+              <div
+                className="rounded-md overflow-hidden focus:outline-none focus:ring-2 focus:ring-blue-500"
+                tabIndex={0}
+              >
                 <header className="bg-[#15191d] h-12 flex items-center">
                   <div className="p-2 cursor-pointer">
-                    <LuLink 
-                      onClick={toggleLinkCustomizer}
-                      size={20} 
-                      className="font-normal" 
-                    />
+                    <LuLink onClick={toggleLinkCustomizer} size={20} className="font-normal" />
                   </div>
                 </header>
                 {showLinkCustomizer && (
-                  <div className="bg-[#1b2024] flex flex-col gap-3 rounded-md shadow-2xl absolute top-1/3 left-1/2 z-10 mb-3 px-8 py-4 w-[350px]" id="input-container">
+                  <div
+                    className="bg-[#1b2024] flex flex-col gap-3 rounded-md shadow-2xl absolute top-1/3 left-1/2 z-10 mb-3 px-8 py-4 w-[350px]"
+                    id="input-container"
+                  >
                     <div className="flex flex-col mb-4">
-                      <label htmlFor="text-label" className="mb-2">Cole um link</label>
+                      <label htmlFor="text-label" className="mb-2">
+                        Cole um link
+                      </label>
                       <input
                         type="text"
                         id="text-label"
@@ -152,7 +170,9 @@ export function App() {
                       />
                     </div>
                     <div className="flex flex-col">
-                      <label htmlFor="link" className="mb-2">Personalize o título do link</label>
+                      <label htmlFor="link" className="mb-2">
+                        Personalize o título do link
+                      </label>
                       <input
                         type="text"
                         id="link"
@@ -163,9 +183,11 @@ export function App() {
                         className="bg-[#22282d] border h-10 p-2 rounded-sm"
                       />
                     </div>
-                    <div className="flex justify-end gap-2 w-full">
-                      <button onClick={toggleLinkCustomizer} className="px-4 py-2">Cancelar</button>
-                      <button 
+                    <div className="flex justify-end gap-2 w-full pt-4">
+                      <button onClick={toggleLinkCustomizer} className="px-4 py-2">
+                        Cancelar
+                      </button>
+                      <button
                         onClick={addLink}
                         className="bg-slate-400 text-[#22282d] px-4 py-2 rounded-md font-semibold"
                       >
@@ -177,24 +199,36 @@ export function App() {
 
                 <div
                   ref={contentRef}
+                  className="focus:outline-none focus:ring-blue-500 focus:border-[3px] focus:border-blue-500"
                   style={{
-                    border: "nome",
                     padding: "10px 20px",
                     minHeight: "150px",
                     cursor: "text",
                     backgroundColor: "#22282d",
                     outline: "none",
-                    height: "300px"
+                    height: "300px",
                   }}
                   suppressContentEditableWarning={true}
                   contentEditable={isEditable}
                 ></div>
               </div>
-            ) 
-            : <p onClick={openContentEditable} className="mt-4 cursor-pointer">
-                Defina um assunto para o card
-              </p>
-          }
+              <div className="flex justify-end gap-2 w-full pt-4">
+                <button onClick={toggleLinkCustomizer} className="px-4 py-2">
+                  Cancelar
+                </button>
+                <button
+                  onClick={addLink}
+                  className="bg-slate-400 text-[#22282d] px-4 py-2 rounded-md font-semibold"
+                >
+                  Salvar
+                </button>
+              </div>
+            </>
+          ) : (
+            <p onClick={openContentEditable} className="mt-4 cursor-pointer">
+              Defina um assunto para o card
+            </p>
+          )}
         </div>
       </div>
     </div>
